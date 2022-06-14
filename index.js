01c72e8952aa9046ebbf29e30c6558b5a9c2b56a
I Ogunseye <77637251+ioguns@users.noreply.github.com>
2022-06-14 20:11:52 +0000

    Created an timestamp microservice following freecodecamp.

--- index.js
+++ index.js
@@ -8,25 +8,64 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get("/", function(req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/hello", function(req, res) {
+  res.json({ greeting: 'hello API' });
+});
+
+// your first API endpoint... 
+app.get("/api/:year-:month-:day", function(req, res) {
+
+  try {
+    const dateFormat = req.params.year + '-' + req.params.month + '-' + req.params.day;
+
+    dateStr = Date.parse(dateFormat);
+
+     if(dNum === NaN){
+      res.json({ error: "Invalid Date" });
+      return;
+    }
+    
+    res.json({ unix: dateStr, utc: (new Date(dateFormat)).toUTCString() });
+  } catch (err) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+
+});
+
+// your first API endpoint... 
+app.get("/api/:date", function(req, res) {
+  utc = undefined;
+  try {
+    dNum = Number.parseInt(req.params.date);
+    if(dNum === NaN){
+      res.json({ error: "Invalid Date" });
+      return;
+    }
+
+    console.log(dNum);
+     res.json({ unix: req.params.date, utc: (new Date(dNum * 1000)).toUTCString() });
+  } catch (err) {
+    res.json({ error: "Invalid Date" });
+    return;
+  }
+ 
 });
 
 
-
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
